34df4cb9a2d040a188048a103e8ba2e3b5b0d519
Philip Stanton <75814801+philDaprogrammer@users.noreply.github.com>
2023-12-02 13:57:00 -0500

    More functional approach

--- day2/day2/Program.cs
+++ day2/day2/Program.cs
@@ -1,94 +1,81 @@
-﻿using System.Data;
+List<Game> parse (string filePath) {
+    var lines = File.ReadAllLines(filePath);  
+    List<Game> games = new List<Game>();
+    
+    List<Dictionary<string, int>> game;
+    Dictionary<string, int> result; 
+    int GameId; 
+    
+    foreach (var line in lines) {
+        var GameIdSplit = line.Split(": "); 
+        
+        GameId = Int32.Parse(GameIdSplit[0].Split(" ")[1]); 
+        game   = new List<Dictionary<string, int>>(); 
+        
+        foreach  (var cubes in GameIdSplit[1].Split("; ")) {
+            result = new Dictionary<string, int>(); 
 
-class Game
-{
+            foreach  (var cube in cubes.Split(", ")) {
+                var cubeSplit = cube.Split(" ");
+                result.Add(cubeSplit[1], Int32.Parse(cubeSplit[0])); 
+            }
+
+            game.Add(result);
+        }
+
+        games.Add(new Game(GameId, game)); 
+    }
+
+    return games;
+}
+
+void part1 (List<Game> games) {
+    Dictionary<string, int> maxAmount = new Dictionary<string, int>() {
+        {"red", 12},
+        {"green", 13},
+        {"blue", 14}
+    };
+
+    int total = games
+        .Where(game => !game.cubes.Any(c => c.Any(c => c.Value > maxAmount[c.Key])))
+            .Sum(game => game.Id);
+
+    Console.WriteLine($"part 1: {total}");  
+}
+
+int maxValue (Game game, string color) {
+    return game.cubes
+        .Where(c => c.ContainsKey(color))
+        .Select(c => c[color])
+        .Max();
+}
+
+int power (Game game) {
+    return maxValue(game, "red") * maxValue(game, "green") * 
+        maxValue(game, "blue"); 
+}
+
+void part2 (List<Game> games) {
+    int total = games
+        .Select(power)
+        .Sum(); 
+
+    Console.WriteLine($"part 2: {total}"); 
+}
+
+// driver code 
+string filePath  = "C:\\Projects\\AOC-2023\\day2\\day2\\input.txt"; 
+
+List<Game> games = parse(filePath);
+part1(games);
+part2(games); 
+
+class Game {
     public int Id { get; set; }
     public List<Dictionary<string, int>> cubes { get; set; }
 
-    public Game(int Id, List<Dictionary<string, int>> cubes)
-    {
+    public Game(int Id, List<Dictionary<string, int>> cubes) {
         this.Id = Id;
         this.cubes = cubes;
-    }
-}
-
-class Solve
-{
-    static List<Game> parse (string filePath)
-    {
-        var lines        = File.ReadAllLines(filePath);  
-        List<Game> games = new List<Game>();
-        List<Dictionary<string, int>> game;
-        int GameId; 
-        
-        foreach (var line in lines)
-        {
-            var GameIdSplit = line.Split(": ");
-
-            GameId = Int32.Parse(GameIdSplit[0].Split(" ")[1]); 
-            game   = new List<Dictionary<string, int>>(); 
-            
-            foreach  (var rolls in GameIdSplit[1].Split("; "))
-            {
-                var cubes = new Dictionary<string, int>(); 
-
-                foreach  (var cube in rolls.Split(", "))
-                {
-                    var cubeSplit = cube.Split(" ");
-                    cubes.Add(cubeSplit[1], Int32.Parse(cubeSplit[0])); 
-                }
-
-                game.Add(cubes);
-            }
-
-            games.Add(new Game(GameId, game)); 
-        }
-
-        return games;
-    }
-
-    static void part1 (List<Game> games)
-    {
-        Dictionary<string, int> total = new Dictionary<string, int>() {
-            {"red", 12},
-            {"green", 13},
-            {"blue", 14}
-        };
-
-        int validGames = 0; 
-        
-        foreach (var game in games)
-        {
-            var invalid = game.cubes.Any(c => c.Any(c => c.Value > total[c.Key]));
-
-            if (!invalid) { validGames += game.Id; }     
-        }
-
-        Console.WriteLine($"total: {validGames}"); 
-    }
-
-    static void part2 (List<Game> games)
-    {
-        int total = 0; 
-        
-        foreach (var game in games)
-        {
-            int red   = game.cubes.Where(c => c.ContainsKey("red")).Select(c => c["red"]).Max();
-            int green = game.cubes.Where(c => c.ContainsKey("green")).Select(c => c["green"]).Max();
-            int blue  = game.cubes.Where(c => c.ContainsKey("blue")).Select(c => c["blue"]).Max(); 
-
-            total += red * green * blue; 
-        }
-
-        Console.WriteLine($"total: {total}"); 
-    }
-
-    public static void Main()
-    {
-        string filePath  = "C:\\Projects\\AOC\\2023\\day2\\day2\\input.txt"; 
-        
-        List<Game> games = parse(filePath);
-        part1(games);
-        part2(games); 
     }
 }
